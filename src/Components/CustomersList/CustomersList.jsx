1dddefb99781b8380f8e99aa2a7b6811eacc1805
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-04-05 19:50:14 +0300

    style pagination

--- src/Components/CustomersList/CustomersList.jsx
+++ src/Components/CustomersList/CustomersList.jsx
@@ -65,9 +65,37 @@ export const CustomersList = ({ user }) => {
             )}
           </tbody>
         </table>
-        <div>
-          <p>Showing data 1 to 8 of 256K entries</p>
-          <div></div>
+        <div className={s.customers__pages}>
+          <p className={s.customers__pages_text}>
+            Showing data 1 to 8 of 256K entries
+          </p>
+          <div className={s.customers__pages_pagination}>
+            <button className={s.customers__pages_pagination_btn} type="button">
+              &lt;
+            </button>
+            <button
+              className={`${s.customers__pages_pagination_btn} ${s.customers__pages_pagination_btn_active}`}
+              type="button"
+            >
+              1
+            </button>
+            <button className={s.customers__pages_pagination_btn} type="button">
+              2
+            </button>
+            <button className={s.customers__pages_pagination_btn} type="button">
+              3
+            </button>
+            <button className={s.customers__pages_pagination_btn} type="button">
+              4
+            </button>
+            <p className={s.customers__pages_pagination_etc}>...</p>
+            <button className={s.customers__pages_pagination_btn} type="button">
+              40
+            </button>
+            <button className={s.customers__pages_pagination_btn} type="button">
+              &gt;
+            </button>
+          </div>
         </div>
       </div>
     </div>
