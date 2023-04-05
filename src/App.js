d97975574e8caf65739daad4722fc341f3267651
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-04-05 15:46:49 +0300

    add sidebar style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { Sidebar } from "./Components/Sidebar/Sidebar";
 import userInfo from "./data/userInfo.json";
 import { CustomersList } from "./Components/CustomersList/CustomersList";
+import s from "./App.module.scss";
 
 function App() {
   const [user, setUser] = useState(userInfo);
   return (
-    <>
+    <div className={s.container}>
       <Sidebar user={user} />
-      <CustomersList user={user}/>
-    </>
+      <CustomersList user={user} />
+    </div>
   );
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { SidebarLink } from "../SidebarLink/SidebarLink";
 import { SidebarUserInfo } from "../SidebarUserInfo/SidebarUserInfo";
+import s from "./Sidebar.module.scss";
 
 export const Sidebar = ({user}) => {
   return (
-    <aside>
-      <h2>Dashboard</h2>
-      <h3>Dashboard</h3>
+    <aside className={s.sidebar}>
+      <h2 className={s.sidebar__title}>Dashboard<span className={s.sidebar__title_vers}>v.01</span></h2>
+      <h3 className={s.sidebarList__title}>Dashboard</h3>
       <SidebarLink />
       <SidebarUserInfo user={user}/>
     </aside>
